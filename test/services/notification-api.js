93e4f30a396d7d4363fc267f1709c5bf8e3b3285
pwadmore-ea <pwadmore-ea@users.noreply.github.cm>
2018-10-19 14:28:38 +0100

    Added more exception related tests

--- test/services/notification-api.js
+++ test/services/notification-api.js
@@ -1,11 +1,13 @@
+const Boom = require('boom')
 const Lab = require('lab')
 const Code = require('code')
 const lab = exports.lab = Lab.script()
 const sinon = require('sinon')
 const notificationService = require('../../server/services/notification-api')
+const restClient = require('../../server/services/rest-client')
 const uuid = require('uuid')
 
-lab.experiment('Home Tests', () => {
+lab.experiment('Notification API Client', () => {
   let sandbox
 
   // Use a Sinon sandbox to manage spies, stubs and mocks for each test.
@@ -18,8 +20,15 @@ lab.experiment('Home Tests', () => {
   })
 
   lab.test('Non-missing resource related exeptions are propagated', async () => {
-    // Spies, mocks and stubs are not used in this test case. If the notification API is not running
-    // a Promise should be rejected.
+    // If the notification API is not running a Promise should be rejected.
     await Code.expect(notificationService.get(uuid.v4())).to.reject()
+    // Stub the REST client rejecting a Promise with a non-Boom error to increase test coverage.
+    sandbox.stub(restClient, 'getJson').rejects(new Error())
+    await Code.expect(notificationService.get(uuid.v4())).to.reject()
+  })
+
+  lab.test('Missing resource related exeptions are not propagated', async () => {
+    sandbox.stub(restClient, 'getJson').rejects(Boom.boomify(new Error(), { statusCode: 404 }))
+    Code.expect(await notificationService.get(uuid.v4())).to.be.undefined()
   })
 })
