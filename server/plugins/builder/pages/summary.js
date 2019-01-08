7e626c7b6c188fb9f6cb5cc087173ec3d95d478e
pwadmore-ea <pwadmore-ea@users.noreply.github.com>
2019-01-08 09:03:27 +0000

    Removed redundant commented code

--- server/plugins/builder/pages/summary.js
+++ server/plugins/builder/pages/summary.js
@@ -97,20 +97,6 @@ class SummaryPage extends Page {
       return h.view('summary', viewModel)
     }
   }
-
-  // get stateSchema () {
-  //   const keys = this.components.getStateSchemaKeys()
-  //   const name = this.components.formItems[0].name
-  //   const d = new Date()
-  //   d.setDate(d.getDate() + 28)
-  //   const max = `${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear()}`
-
-  //   // Extend the key to validate that the date is
-  //   // greater than today and less than today+28 days
-  //   keys[name] = keys[name].min('now').max(max)
-
-  //   return joi.object().keys(keys)
-  // }
 }
 
 module.exports = SummaryPage
